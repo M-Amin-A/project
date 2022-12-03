8ce5a5a1c6f86e564d45ea44936025c925d2d9ef
M.Amin <m.a.abbasfar@gmail.com>
2022-12-03 09:54:15 +0330

    release 1.1 on branch1

--- project.c
+++ project.c
@@ -7,9 +7,16 @@ int main()
 
     for(int i=1;i<=n;i++)
     {
-        for(int j=0;j<i;j++)
+        for(int j=1;j<=n;j++)
         {
-            printf("*");
+            if(j<=i)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf("^");
+            }
         }
         printf("\n");
     }
